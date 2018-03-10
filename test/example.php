288d0c88062aa3a34bf330500eac065ecc1cdbb5
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2018-03-10 23:47:58 +0100

    example1.php
    Create file with some variables, arrays and loops

---
+++ test/example.php
@@ -0,0 +1,35 @@
+<?php
+/*
+// $myVar = "Hello";
+// $myVar2 = " World!";
+// echo $myVar. '' . $myVar2;
+
+$numbers = array(1, 2, 3, 4, 5);
+
+//print_r($numbers);
+
+//echo $numbers[1];
+
+$ages = array(
+	"Dragan" => 44,
+	"Marko" => 40,
+	"Boban" => 50
+);
+
+//echo $ages['Boban'];
+
+echo count($ages);
+*/
+// $i = 0;
+// 	while ($i <= 10) {
+// 		echo 'Number ' . $i . '<br />';
+// 		$i++;
+// 	}
+
+$numbers = array(1, 2, 3, 4, 5);
+
+	foreach($numbers as $number) {
+		echo 'This is number ' . $number . '<br />';
+	}
+
+?>
