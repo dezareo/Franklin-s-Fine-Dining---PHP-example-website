fd76a82ee1f8cdfbcc8785dcd58068aa690300bd
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-12-30 21:48:42 +0100

    Created footer.php
    footerr.php included html code from index.php

---
+++ assets/includes/footer.php
@@ -0,0 +1,41 @@
+<div id="footer" class="cf">
+
+				<div class="column three">
+					<strong>Phone</strong>
+					064-064-064
+				</div><!-- column three -->
+
+				<div class="column three">
+					<strong>Location</strong>
+					123 Sunset Boulevard
+					L.A. California
+				</div><!-- column three -->
+
+				<div class="column three last">
+
+					<strong>Hours</strong>
+					<em>Tuesday - Thursday</em><br>
+					1:00pm - 9:00pm<br><br>
+
+					<em>Friday - Saturday</em><br>
+					4:00pm - 11:00pm<br><br>
+
+					<em>Sunday - Monday</em><br>
+					Closed<br>
+
+				</div><!-- column three -->
+
+			</div><!-- footer -->
+
+			<small>@copy 2020 created by Dezareo</small>
+
+		</div><!-- content -->
+
+	</div><!-- wrapper -->
+
+	<div class="copyright-info">
+		<?php include('./assets/includes/copyright.php'); ?>
+	</div>
+
+</body>
+</html>
