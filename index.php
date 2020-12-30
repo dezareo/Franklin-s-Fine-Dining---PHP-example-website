8076285cd582c56aa2bc3385f94b641e4a5f08b6
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-12-30 19:43:08 +0100

    Create basic html layout for index page
    Create all basic laout for main index page

--- index.php
+++ index.php
@@ -2,14 +2,60 @@
 <html>
 	<head>
 		<title>Index Page</title>
+		<link rel="stylesheet" type="text/css" href="/assets/styles.css">
 	</head>
-<body>
-	<?php
-	//this is first php script
-	echo "Hello World!";
+<body id="final-example">
 
-	?>
+	<div class="wrapper">
 
+		<div id="banner">
+
+		</div><!-- banner -->
+
+		<div id="nav">
+
+		</div><!-- nav -->
+
+		<div class="content">
+
+			<div id="footer" class="cf">
+
+				<div class="column three">
+					<strong>Phone</strong>
+					064-064-064
+				</div><!-- column three -->
+
+				<div class="column three">
+					<strong>Location</strong>
+					123 Sunset Boulevard
+					L.A. California
+				</div><!-- column three -->
+
+				<div class="column three last">
+
+					<strong>Hours</strong>
+					<em>Tuesday - Thursday</em><br>
+					1:00pm - 9:00pm<br><br>
+
+					<em>Friday - Saturday</em><br>
+					4:00pm - 11:00pm<br><br>
+
+					<em>Sunday - Monday</em><br>
+					Closed<br>
+
+				</div><!-- column three -->
+
+			</div><!-- footer -->
+
+			<small>@copy 2020 created by Dezareo</small>
+
+		</div><!-- content -->
+
+	</div><!-- wrapper -->
+
+	<div class="copyright-info">
+		<?php include('../assets/includes/copyright.php'); ?>
+	</div>
 
 </body>
 </html>
