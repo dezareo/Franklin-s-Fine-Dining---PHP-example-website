06e0e8ba3b34c82ae0c90ae527f2183d7e46691d
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-12-30 21:48:21 +0100

    Created header.php
    header.php included html code from index.php

---
+++ assets/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Index Page</title>
+		<link rel="stylesheet" type="text/css" href="/assets/styles.css">
+	</head>
+<body id="final-example">
+
+	<div class="wrapper">
+
+		<div id="banner">
+			<a href="/" title="Return to Home">
+				<img src="./assets/img/banner.png" alt="Franklin's Fine Dining">
+			</a>
+		</div><!-- banner -->
+
+		<div id="nav">
+			<?php include('./assets/includes/nav.php'); ?>
+		</div><!-- nav -->
+
+		<div class="content">
