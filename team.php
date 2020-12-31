7fb859d7f873554631dc5bfaa5f2b41b489c4bd6
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-12-31 19:19:18 +0100

    Create team.php

---
+++ team.php
@@ -0,0 +1,13 @@
+<?php
+define("TITLE", "Team | Franklin\'s Fine Dining");
+include './assets/includes/header.php';
+?>
+
+	<div id="team-members" class="cf">
+
+	</div><!-- team-members -->
+
+<?php include './assets/includes/footer.php';?>
+
+
+
