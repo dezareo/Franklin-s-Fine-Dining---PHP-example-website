cd1c2ea84eecf057b5d62d25ac4897416ac82bb0
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-12-31 23:22:15 +0100

    Update arrays.php
    Create $menuItems array into array.php page

--- assets/includes/arrays.php
+++ assets/includes/arrays.php
@@ -5,18 +5,22 @@
 $navItems = array(
 
 	array(
+
 		'slug' => 'index.php',
 		'title' => 'Home',
 	),
 	array(
+
 		'slug' => 'team.php',
 		'title' => 'Team',
 	),
 	array(
+
 		'slug' => 'menu.php',
 		'title' => 'Menu',
 	),
 	array(
+
 		'slug' => 'contact.php',
 		'title' => 'Contact',
 	),
@@ -28,22 +32,60 @@ $navItems = array(
 $teamMembers = array(
 
 	array(
+
 		"name" => "Frankie",
 		'position' => 'Owner',
 		'bio' => 'Frankie is the gread-grandson of the original Franklin. He is the owner of Franklin\'s Fine Dining.',
 		'img' => 'frankie',
 	),
 	array(
+
 		'name' => 'Francis',
 		'position' => 'General Manager',
 		'bio' => 'Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don\'t miss her Margherita Mondays!',
 		'img' => 'francis',
 	),
 	array(
+
 		'name' => 'Carlos',
 		'position' => 'Head Chef',
 		'bio' => 'Carlos is the epitome of the phrase &ldquo;Don\'t judge a book by it\'s cover&fdquo; &mdash; You simply cannot find a better chef.',
 		'img' => 'carlos',
+	),
+
+);
+
+//Menu Items
+
+$menuItems = array(
+
+	"club-sandwich" => array(
+
+		'title' => 'Club Sandwich',
+		'price' => 11,
+		'blurb' => ' lorem ipsum some text we put in there ... ',
+		'dring' => 'Club Soda',
+	),
+	"dill-salmon" => array(
+
+		'title' => 'Lemon &amp; Dill Salmon',
+		'price' => 30,
+		'blurb' => ' lorem ipsum some text we put in there ... ',
+		'dring' => 'White Wine',
+	),
+	"super-salad" => array(
+
+		'title' => 'Cesar Salad',
+		'price' => 8,
+		'blurb' => ' lorem ipsum some text we put in there ... ',
+		'dring' => 'Mineral Water',
+	),
+	"mexican-barbacoa" => array(
+
+		'title' => 'Mexican Barbacoa',
+		'price' => 20,
+		'blurb' => ' lorem ipsum some text we put in there ... ',
+		'dring' => 'Coca Cola',
 	),
 
 );
