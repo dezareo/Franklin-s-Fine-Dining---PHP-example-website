98141aa30c5ffdff01624d34946ac4625a2fb2e2
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-12-31 21:21:18 +0100

    Create $teamMembers array into arrays.php

--- assets/includes/arrays.php
+++ assets/includes/arrays.php
@@ -5,20 +5,45 @@
 $navItems = array(
 
 	array(
-		'slug' 	=> 'index.php',
-		'title' 	=> 'Home'
+		'slug' => 'index.php',
+		'title' => 'Home',
 	),
 	array(
-		'slug' 	=> 'team.php',
-		'title' 	=> 'Team'
+		'slug' => 'team.php',
+		'title' => 'Team',
 	),
 	array(
-		'slug' 	=> 'menu.php',
-		'title' 	=> 'Menu'
+		'slug' => 'menu.php',
+		'title' => 'Menu',
 	),
 	array(
-		'slug' 	=> 'contact.php',
-		'title' 	=> 'Contact'
+		'slug' => 'contact.php',
+		'title' => 'Contact',
+	),
+
+);
+
+//Team Members
+
+$teamMembers = array(
+
+	array(
+		"name" => "Frankie",
+		'position' => 'Owner',
+		'bio' => 'Frankie is the gread-grandson of the original Franklin. He is the owner of Franklin\'s Fine Dining.',
+		'img' => 'frankie',
+	),
+	array(
+		'name' => 'Francis',
+		'position' => 'General Manager',
+		'bio' => 'Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don\'t miss her Margherita Mondays!',
+		'img' => 'francis',
+	),
+	array(
+		'name' => 'Carlos',
+		'position' => 'Head Chef',
+		'bio' => 'Carlos is the epitome of the phrase &ldquo;Don\'t judge a book by it\'s cover&fdquo; &mdash; You simply cannot find a better chef.',
+		'img' => 'carlos',
 	),
 
 );
