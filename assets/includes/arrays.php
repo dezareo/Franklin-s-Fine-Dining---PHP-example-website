8a006cfba18a9bb317975687029376a594c6f2ab
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-01 15:26:21 +0100

    Update arrays.php
    Add text into blurb.

--- assets/includes/arrays.php
+++ assets/includes/arrays.php
@@ -63,29 +63,31 @@ $menuItems = array(
 
 		'title' => 'Club Sandwich',
 		'price' => 11,
-		'blurb' => ' lorem ipsum some text we put in there ... ',
-		'dring' => 'Club Soda',
+		'blurb' => ' A "good" sandwich. With the exception of the third peice of bread (in the middle of the sandwich) - its much like the sandwiches I make normally. ',
+		'drink' => 'Club Soda',
 	),
 	"dill-salmon" => array(
 
 		'title' => 'Lemon &amp; Dill Salmon',
 		'price' => 30,
-		'blurb' => ' lorem ipsum some text we put in there ... ',
-		'dring' => 'White Wine',
+		'blurb' => ' This is a wonderful (and easy) recipe I got from my Aunt. The flavors of lemon and dill really complement the fish. I like to use wild Alaskan salmon for this recipe, but any salmon will work. I usually don\'t measure, so these are all estimates. Play with the ingredients to suit your own tastes! ',
+		'drink' => 'White Wine',
 	),
 	"super-salad" => array(
 
 		'title' => 'Cesar Salad',
 		'price' => 8,
-		'blurb' => ' lorem ipsum some text we put in there ... ',
-		'dring' => 'Mineral Water',
+		'blurb' => ' A Caesar salad is a green salad of romaine lettuce and croutons dressed with lemon juice, olive oil, egg, Worcestershire sauce, anchovies, garlic, Dijon mustard, Parmesan cheese, and black pepper. ',
+		'drink' => 'Mineral Water',
 	),
 	"mexican-barbacoa" => array(
 
 		'title' => 'Mexican Barbacoa',
 		'price' => 20,
-		'blurb' => ' lorem ipsum some text we put in there ... ',
-		'dring' => 'Coca Cola',
+		'blurb' => ' A simple barbacoa, slow cooked and seasoned with chiles and spices. Serve on tortillas with salsa, rice, beans or other accoutrements.
+
+ ',
+		'drink' => 'Coca Cola',
 	),
 
 );
