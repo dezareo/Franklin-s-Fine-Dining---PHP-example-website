93eac09e336c449d03b142862e4acd5f5e685533
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-01 18:01:44 +0100

    create contact.php

--- contact.php
+++ contact.php
@@ -10,6 +10,66 @@ include './assets/includes/header.php';?>
 
 		<form method="post" action="" id="contact-form">
 
+			<?php
+
+//check for header injection
+function has_header_injection($str) {
+	return preg_match('/[\r\n]/', $str);
+}
+
+if (isset($_POST['contact_submit'])) {
+
+	$name = trim($_POST['name']);
+	$email = trim($_POST['email']);
+	$message = $_POST['message'];
+
+	//Check to see if $name or $email have header injections
+	if (has_header_injection($name) || has_header_injection($email)) {
+		die(); //If true, kill the script
+	}
+
+	if (!$name || !$email || !$message) {
+		echo '<h4 class="error">All fields required.</h4><a href="contact.php" class="button block">Go back and try again</a>';
+		exit;
+	}
+
+	//Add the recipient email to a variable
+	$to = "dezareo@gmail.com";
+
+	//Create a subject
+	$subject = "$name sent you a message via your contact form";
+
+	//Construct the message
+	$message = 'Name: $name\r\n';
+	$message = 'Email: $email\r\n';
+	$message = 'Message:\r\n$message';
+
+	//If the subscribe checkbox was checked ...
+	if (isset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe') {
+
+		//Add a new line to the $message
+		$message .= '\r\n\r\nPlease add $email to the mailing list.\r\n';
+	}
+
+	$message = wordwrap($message, 72);
+
+	//Set the mail headers into a variable
+	$headers = 'MIME-Version: 1.0\r\n';
+	$headers .= 'Content-type: text/plan; charset=iso-8859-1\r\n';
+	$headers .= 'From: $name <$email>\r\n';
+	$headers .= 'X-MSMail-Priority: High\r\n\r\n';
+
+	//Send the email
+	mail($to, $subject, $message, $headers);
+
+	?>
+	<!-- Show success message after mail has sent -->
+	<h5>Thansk for contacting us.</h5>
+	<p>Please allow 24 hours for response</p>
+	<p><a href="index.php" class="button block">&laquo; Go to Home Page</a></p>
+
+	<?php } else {?>
+
 			<label for="name">Your name</label>
 			<input type="text" id="name" name="name">
 
@@ -26,6 +86,8 @@ include './assets/includes/header.php';?>
 			<input type="submit" class="button next" name="contact_submit" value="Send Message">
 
 		</form>
+
+	<?php }?>
 
 		<hr>
 
